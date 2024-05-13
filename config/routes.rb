876e38e397b71ba563f3c7d71a5cb7cc413ee512
Jessica Chen <jiangyuechen2024@u.northwestern.edu>
2024-05-13 00:37:57 +0000

    final-ver

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html  
   resources "places"
-  get("/places", :controller => "places", :action => "index")
+  #get("/places", :controller => "places", :action => "index")
   resources "entries"
-  get("/entries", :controller => "entries", :action => "index")
+  #get("/entries", :controller => "entries", :action => "index")
 
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+  get("/", { :controller => "places", :action => "index" })
 end
